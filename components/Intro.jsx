19554c5079e09334679d94a503d1a0ee9a3f64fe
Hamees Sayed <hameessayed71@gmail.com>
2022-12-27 17:18:43 +0530

    intro page complete

---
+++ components/Intro.jsx
@@ -0,0 +1,9 @@
+export default function Intro() {
+  return (
+    <div className="front--page">
+      <h1 className="title">Quizzical</h1>
+      <h3 className="desc">Test your knowledge on a wide range of topics. From History and Science to Mythology and Sports. If you're a trivia fanatic looking for a challenge, or just want to learn something new, Come On in and Give it a Try!</h3>
+      <button className="start--quiz">Start Quiz</button>
+    </div>
+  )
+}
